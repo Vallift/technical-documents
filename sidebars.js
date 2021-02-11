416a30fec2d664115b5d7b4a964eef8c0b5a5d10
Nandha <nandhakumargdr@gmail.com>
2021-02-11 19:48:12 +0530

    Initial

---
+++ sidebars.js
@@ -0,0 +1,10 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Installation Guides',
+      items: ['installation-guide/keycloak-docker-nginx-setup', 'others'],
+    }
+  ],
+
+};
